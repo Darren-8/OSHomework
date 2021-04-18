d2c6d53793b7e8021e8c12fbe4f16f5526dc1bc6
FaridatZiyu <ziyu_nku@qq.com>
2021-04-18 19:05:30 +0800

    new file:   lab2/kern/mm/buddy_pmm.h

---
+++ lab2/kern/mm/buddy_pmm.h
@@ -0,0 +1,9 @@
+#ifndef __KERN_MM_BUDDY_PMM_H__
+#define  __KERN_MM_BUDDY_PMM_H__
+
+#include <pmm.h>
+
+extern const struct pmm_manager default_buddy_manager;
+
+#endif /* ! __KERN_MM_BUDDY_PMM_H__ */
+
