b01efa72d7767c38fa54bb98587674919e29d258
FaridatZiyu <ziyu_nku@qq.com>
2021-04-19 09:46:49 +0800

    completed buddy

--- lab2/kern/mm/buddy_pmm.c
+++ lab2/kern/mm/buddy_pmm.c
@@ -7,9 +7,36 @@
 static unsigned int* buddy_page; // 存当前段内最长可供分配的连续内存块大小
 static unsigned int buddy_page_num; // 存储buddy本身需要的页
 static unsigned int max_pages; // buddy储存的页，叶节点数
-static struct Page* buddy_allocatable_base; // 叶节点首地址
+static struct Page* buddy_allocatable_base; // 页首地址
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
+
+/*
+zkw线段树有一些很好的性质
+0  1         0       1
+ 2   3   ->     10       11
+4 5 6 7      100  101 110  111
+下标为 n
+子节点为 n<<1 和 n<<1|1
+父节点为 n>>1
+
+init:
+       8
+   4       4
+ 2   2   2   2
+1 1 1 1 1 1 1 1
+
+allocate/free:
+       4
+   2       4
+ 0   2   2   2
+0 0 1 1 1 1 1 1
+
+       4
+   1       4
+ 0   1   2   2
+0 0 0 1 1 1 1 1
+*/
 
 static void
 buddy_init(void) {}
@@ -34,7 +61,7 @@ buddy_init_memmap(struct Page *base, size_t n) {
     for (int i = 0; i < buddy_page_num; ++i)
         SetPageReserved(base + i);
     
-    // 将叶节点，即之后的max_pages个页设置为property
+    // 将之后的max_pages个页设置为property
     buddy_allocatable_base = base + buddy_page_num;
     for (struct Page *p = buddy_allocatable_base; p != base + n; ++p) {
         ClearPageReserved(p);
@@ -53,7 +80,9 @@ buddy_init_memmap(struct Page *base, size_t n) {
 static struct
 Page* buddy_alloc_pages(size_t n) {
     assert(n > 0);
-    
+
+    // 左子节点优先地、logn地搜索最紧的节点
+    // index是该节点对应下标，size是相应大小
     if (n > buddy_page[1]) return NULL;
     unsigned int index = 1, size = max_pages;
     for (; size >= n; size >>= 1) {
@@ -62,11 +91,17 @@ Page* buddy_alloc_pages(size_t n) {
         else break;
     }
     buddy_page[index] = 0;
-    // allocate all pages under node[index]
+
+    // 从下标index开始分配size个页
     struct Page* new_page = buddy_allocatable_base + index * size - max_pages;
-    for (struct Page* p = new_page; p != new_page + size; ++p)
-        set_page_ref(p, 0), ClearPageProperty(p);
-    for (; (index >>= 1) > 0; ) // since destory continuous, use MAX instead of SUM
+    for (struct Page* p = new_page; p != new_page + size; ++p) {
+        set_page_ref(p, 0);
+        ClearPageProperty(p);
+    }
+
+    // 反向更新父节点
+    // 一旦某节点下的一个后代节点开始分配，其被更新为子节点的MAX而非SUM
+    for (; (index >>= 1) > 0; )
         buddy_page[index] = max(buddy_page[index << 1], buddy_page[index << 1 | 1]);
     return new_page;
 }
@@ -74,21 +109,33 @@ Page* buddy_alloc_pages(size_t n) {
 static void
 buddy_free_pages(struct Page *base, size_t n) {
     assert(n > 0);
-    unsigned int index = (unsigned int)(base - buddy_allocatable_base) + max_pages, size = 1;
-    // find first buddy node which has buddy_page[index] == 0
-    for (; buddy_page[index] > 0; index >>= 1, size <<= 1);
-    // free all pages
+
+    // 将base映射到buddy[index]上
+    unsigned int index = (unsigned int)(base - buddy_allocatable_base) + max_pages;
+    unsigned int size = 1;
+
+    // free所有页
     for (struct Page *p = base; p != base + n; ++p) {
         assert(!PageReserved(p) && !PageProperty(p));
-        SetPageProperty(p), set_page_ref(p, 0);
+        SetPageProperty(p);
+        set_page_ref(p, 0);
     }
-    // modify buddy_page
+
+    // 向上找到buddy_page[index] == 0的最高节点
+    for (; buddy_page[index] > 0; index >>= 1, size <<= 1);
+
+    // 向上调整buddy_page
     for (buddy_page[index] = size; size <<= 1, (index >>= 1) > 0;)
-        buddy_page[index] = (buddy_page[index << 1] + buddy_page[index << 1 | 1] == size) ? size : max(buddy_page[index << 1], buddy_page[index << 1 | 1]);
+        buddy_page[index] =
+            (buddy_page[index << 1] + buddy_page[index << 1 | 1] == size) ?
+            size :
+            max(buddy_page[index << 1], buddy_page[index << 1 | 1]);
 }
 
 static size_t
-buddy_nr_free_pages(void) { return buddy_page[1]; }
+buddy_nr_free_pages(void) {
+    return buddy_page[1];
+}
 
 static void
 buddy_check(void) {
